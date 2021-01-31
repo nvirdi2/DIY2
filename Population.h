cd3504d237c7e8e7d74b6f4652882c3a0bcc8af5
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-01-31 18:49:27 -0500

    Create Population.h

---
+++ Population.h
@@ -0,0 +1,30 @@
+/// Name: Navdeep Virdi
+// Seneca email: nvirdi2@myseneca.ca
+// Student ID: 166485193
+// Date: Jan 31, 2021
+
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+#ifndef SDDS_POPULATION_H_
+#define SDDS_POPULATION_H_
+#define DATAFILE "PCpopulations.csv"
+
+namespace sdds 
+{
+    struct PopulationPC {
+            char* PostalCode;
+            int Population;
+        };
+
+    void sort();
+
+	bool load();
+    bool load(PopulationPC& postalcodePOP);
+
+	void display();
+    void display(const PopulationPC& postalcodePOP);
+
+	void deallocateMemory();
+
+}
+#endif // SDDS_POPULATION_H_
