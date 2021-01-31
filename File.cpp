4766e9aade930f458bfc2a9548e3ac6453e53213
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-01-31 18:47:37 -0500

    Create File.cpp

---
+++ File.cpp
@@ -0,0 +1,44 @@
+/// Name: Navdeep Virdi
+// Seneca email: nvirdi2@myseneca.ca
+// Student ID: 166485193
+// Date: Jan 31, 2021
+
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include "File.h"
+
+#include <iostream>
+
+namespace sdds {
+   FILE* fptr;
+   bool openFile(const char filename[]) {
+      fptr = fopen(filename, "r");
+      return fptr != NULL;
+   }
+   int noOfRecords() {
+      int noOfRecs = 0;
+      char ch;
+      while (fscanf(fptr, "%c", &ch) == 1) {
+         noOfRecs += (ch == '\n');
+      }
+      rewind(fptr);
+      return noOfRecs;
+   }
+   void closeFile() {
+      if (fptr) fclose(fptr);
+   }
+
+   bool read(int &population)
+	{
+		bool complete = fscanf(fptr, "%d\n", &population);
+		return complete;
+	}
+
+   bool read(char* Postal_Code)
+	{
+		bool complete = fscanf(fptr, "%3s,", Postal_Code);
+		return complete;
+	}
+}
